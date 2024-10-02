5b5f4f3fbb3b0c180db3222872a0ea25613c64ad
Cynthia Mutie <c.mutie@alustudentcom>
2024-10-02 07:41:13 -0700

    main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# main.py
+# Entry point for performing operations on sparse matrices
+
+from sparse_matrix import SparseMatrix
+
+def main():
+    print("Sparse Matrix Operations")
+
+if __name__ == "__main__":
+    main()
+
